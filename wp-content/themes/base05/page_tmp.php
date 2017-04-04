39085e5e2af9d4952e183b326ee7c3c0f153307a
Gruel Nicolas <gruelnicolas@macbookpro-de-akatouka.home>
2017-04-04 10:28:45 +0200

    robot + no follow

--- wp-content/themes/base05/page_tmp.php
+++ wp-content/themes/base05/page_tmp.php
@@ -10,7 +10,7 @@
 	<title>Ceysson</title>
     <meta name="viewport" content="width=device-width" />
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	
+	<meta name=”robot” content=”noindex, nofollow”>
 </head>
 <body>
 
