03678b1b448c6ca3a92e804d8935e7b973645514
Gruel Nicolas <gruelnicolas@macbookpro-de-akatouka.home>
2017-04-04 16:22:12 +0200

    [4H] pige technique + mail corrections après réunion

--- wp-content/themes/base05/category.php
+++ wp-content/themes/base05/category.php
@@ -24,7 +24,7 @@ the_post_thumbnail( array(100,100) );
 <?php endif;?>
 
 <section class="gallery block-center center">
-<?php echo do_shortcode( '[cactus-masonry quality="full" post_category="IAC" display_post_titles="true" max_width="157px" max_height="600px" horizontal_spacing="17" vertical_spacing="17"]' );?>
+<?php echo do_shortcode( '[cactus-masonry quality="large" post_category="IAC" display_post_titles="true" max_width="157px" max_height="600px" horizontal_spacing="17" vertical_spacing="17" infinite_scroll="false" posts_per_page="2" show_loader="false"]' );?>
     <!-- <?php if(have_posts()) : ?>
       <?php $i=1; while(have_posts()) : the_post(); ?>
         <div id="actu-0<?php echo ($i++); ?>" class="thumb-wrapper">
